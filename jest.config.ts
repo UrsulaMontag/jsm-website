bf9c0e688fe2a95532e7cbbc5cee26b694427ceb
ugla <montagu666@gmail.com>
2024-12-15 18:45:14 +0100

    Implement unittests for LocaleSwitcherSelect.tsx

--- jest.config.ts
+++ jest.config.ts
@@ -97,7 +97,7 @@ const config: Config = {
     // ],
 
     // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-    // moduleNameMapper: {},
+    moduleNameMapper: {'^@/(.*)$': '<rootDir>/src/$1',},
 
     // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
     // modulePathIgnorePatterns: [],
@@ -109,7 +109,7 @@ const config: Config = {
     // notifyMode: "failure-change",
 
     // A preset that is used as a base for Jest's configuration
-    // preset: undefined,
+    preset: 'ts-jest',
 
     // Run tests from one or more projects
     // projects: undefined,
@@ -153,7 +153,7 @@ const config: Config = {
     // snapshotSerializers: [],
 
     // The test environment that will be used for testing
-    testEnvironment: "jest-environment-jsdom",
+    testEnvironment: "jsdom",
 
     // Options that will be passed to the testEnvironment
     // testEnvironmentOptions: {},
