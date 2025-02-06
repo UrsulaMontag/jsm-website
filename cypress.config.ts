915be1492fdb1189cb506bd04e19d971e7e9877f
ugla <montagu666@gmail.com>
2025-02-06 14:59:22 +0100

    Finally fix cy tests to also run in production and with every system theme. Declare Cypress types.

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,8 +3,16 @@ import {defineConfig} from 'cypress';
 export default defineConfig({
     projectId: "zu2j3s",
     e2e: {
-        // eslint-disable-next-line
         setupNodeEvents(_on: any, _config: any) {
+
+            interface AUTWindow extends Window {
+                localStorage: Storage;
+                matchMedia: (query: string) => MediaQueryList;
+            }
+
+            interface VisitOptions {
+                onBeforeLoad?: ((win: AUTWindow) => void) | ((...args: any[]) => void);
+            }
         },
         baseUrl: process.env.CYPRESS_BASE_URL || 'http://localhost:3000',
         viewportWidth: 1280,
