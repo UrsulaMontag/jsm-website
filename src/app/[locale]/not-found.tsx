7151b20c7cc79de23c82ba48c0ccbee180d83e5b
ugla <montagu666@gmail.com>
2025-02-10 18:41:58 +0100

    Add translation to custom 404

--- src/app/[locale]/not-found.tsx
+++ src/app/[locale]/not-found.tsx
@@ -1,18 +1,19 @@
 import {Link} from "@/i18n/routing";
+import {useTranslations} from "next-intl";
 
 export default function NotFound() {
+    const t = useTranslations('ErrorPage');
 
     return (
-        <section
-            className=" flex flex-col items-center justify-center h-screen bg-neutral-beige dark:bg-dark-bg z-10">
-            <h1 className="text-5xl font-bold text-lake-blue dark:text-dark-highlight mb-8">Page Not Found</h1>
-            <p className="text-lg text-center mb-6">The page you are looking for does not exist.</p>
-            <p className="text-lg text-center mb-8">This page will be available soon.</p>
+        <section className="flex flex-col items-center justify-center h-screen bg-neutral-beige dark:bg-dark-bg z-10">
+            <h1 className="text-5xl font-bold text-lake-blue dark:text-dark-highlight mb-8">{t('title')}</h1>
+            <p className="text-lg text-center mb-6">{t('description')}</p>
+            <p className="text-lg text-center mb-8">{t('comingSoon')}</p>
 
-            {/* Link back to homepage or other useful page */}
-            <Link href="/" aria-label="Go back to the homepage"
+            {/* Link back to homepage */}
+            <Link href="/"
                   className="inline-block bg-sunset-orange hover:bg-sunset-orange-dark text-white text-lg py-3 px-6 rounded-lg shadow-lg hover:shadow-xl">
-                Go back to the homepage
+                {t('backToHome')}
             </Link>
         </section>
     );
