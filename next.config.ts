ebafe038e37cd0df71dcd4f05f91de701bfb65ca
ugla <montagu666@gmail.com>
2025-02-10 17:21:08 +0100

    refactor config

--- next.config.ts
+++ next.config.ts
@@ -16,10 +16,7 @@ const nextConfig: NextConfig = {
         loader: 'cloudinary',
         path: 'https://res.cloudinary.com/montagu666/image/upload/',
     },
-    experimental: {
-        optimizeCss: true,
-    },
-    productionBrowserSourceMaps: true,
+   
 };
 
 export default withNextIntl(nextConfig);
