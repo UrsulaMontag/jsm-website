49ad1ce58fe63a3447f66aacdc8aba17916da658
ugla <montagu666@gmail.com>
2025-02-13 16:11:55 +0100

    Simplify root-not-found

---
+++ src/app/root-not-found.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+export default function RootNotFound() {
+    return (
+        <div className="min-h-screen bg-neutral-beige dark:bg-dark-bg flex items-center justify-center">
+            <h1 className="text-4xl text-lake-blue dark:text-dark-highlight">
+                404 - Page Not Found
+            </h1>
+        </div>
+    );
+}
