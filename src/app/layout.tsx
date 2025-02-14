17ab124e84713d8f11a663ea9740e66bc58e5752
ugla <montagu666@gmail.com>
2025-02-14 10:23:24 +0100

    Delete stupid readonly in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,17 +6,19 @@ import {NextIntlClientProvider} from "next-intl";
 import {getMessages} from "next-intl/server";
 import {Locale} from "@/i18n/routing";
 import {pick} from "lodash";
+import {getLayoutProps} from "@/app/[locale]/rootLayoutProps";
 
 type LayoutProps = {
     children: ReactNode;
-    params: { locale: Locale; };
+    params: Promise<{ locale: Locale; }>;
 };
 export default async function RootLayout({children, params}: LayoutProps) {
-    const messages = await getMessages({locale: params.locale});
-
+    const messages = await getMessages();
+    const resolvedParams = await params;
+    const {locale} = await getLayoutProps(resolvedParams);
     return (
         <NextIntlClientProvider
-            locale={params.locale}
+            locale={locale}
             // Make sure to provide at least the messages for `Error`
             messages={pick(messages, 'Error')}
         >
